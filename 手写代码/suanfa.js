2999967312dd90193e6dad06c12179b36711015b
Yincheng <974578444@qq.com>
2021-10-12 02:19:23 +0800

    fighting

---
+++ 手写代码/suanfa.js
@@ -0,0 +1,4 @@
+
+let a = [1,2,3,4]
+let b = a.map((item) => item + 1)
+console.log(b)
--- 测试.js
+++ 测试.js
@@ -1,14 +1,16 @@
-function Parent(name,age){
-    this.name=name
-    this.age=age
-    console.log(this)
-    console.log(this.name,this.age)
-}
-function Children(name,age,height){
-    console.log(this,arguments)
-    // Parent.apply(this,[name,age,height])
-    // Parent(name,age)
-    this.height=height
-}
-let lisi=new Children("李四",12,170)
-// console.log(lisi.name,lisi.age,lisi.height)
+var isValid = function(s) {
+    const stack = [],
+          map = {
+              "(":")",
+              "[":"]",
+              "{":"}"
+          };
+     for(const x of s) {
+         if(x in map) {
+             stack.push(x);
+             continue;
+         };
+         if(map[stack.pop() !== x]) return false
+     }
+     return !stack.length
+ };
